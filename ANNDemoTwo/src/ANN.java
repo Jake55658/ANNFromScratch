8a9e6d67fa0a1d5d6d0a02dffbff6f0b3c310d4e
Aidin Yazdi <aidinyazdi@gmail.com>
2020-07-21 16:35:46 -0500

    make the ANN get input values, normalize the inputs, calculate output values, and then denormalize the output values (no backpropagation yet)

--- ANNDemoTwo/src/ANN.java
+++ ANNDemoTwo/src/ANN.java
@@ -1,76 +1,226 @@
 import java.util.Random;
 
 public class ANN {
+	// a method to output a String to represent a given array
+	public String arrToString(double[] arr) {
+		// initialize the string
+		String arrayString = "";
+		// add to "arrayString" for every element in arr[]
+		for (int i = 0; i < arr.length; i++) {
+			arrayString = arrayString + " [" + arr[i] + "],";
+		}
+		// return "arrayString"
+		return arrayString;
+	}
+
 	// initializing the layers
-	static int inputLayerSize, hiddenLayerOneSize, hiddenLayerTwoSize, outputLayerSize;
-	
-	// private classes for changing the layers
-	private static void changeInputLayerSize (int newInputLayerSize) {
+	int inputLayerSize, hiddenLayerOneSize, hiddenLayerTwoSize, outputLayerSize;
+	// initializing the biases
+	double[] biasesLayerOne, biasesLayerTwo, biasesOutputLayer;
+	// initializing the weights
+	double[][] weightsAfterInputLayer, weightsAfterLayerOne, weightsAfterLayerTwo;
+	// initializing the nodes
+	double[] hiddenLayerOneNodes, hiddenLayerTwoNodes;
+
+	// a boolean to keep track of if we normalized the data or not
+	boolean normalized = false;
+
+	// variables to keep track of normalization and denormalization
+	double minInput, maxInput, minOutput, maxOutput;
+
+	// private methods for changing the layers
+	private void changeInputLayerSize(int newInputLayerSize) {
 		if (newInputLayerSize <= 0) {
 			throw new ArithmeticException("layers must have more than one node");
 		} else {
 			inputLayerSize = newInputLayerSize;
 		}
 	}
-	private static void changeHiddenLayerOneSize (int newHiddenLayerOneSize) {
+
+	private void changeHiddenLayerOneSize(int newHiddenLayerOneSize) {
 		if (newHiddenLayerOneSize <= 0) {
 			throw new ArithmeticException("layers must have more than one node");
 		} else {
 			hiddenLayerOneSize = newHiddenLayerOneSize;
 		}
 	}
-	private static void changeHiddenLayerTwoSize (int newHiddenLayerTwoSize) {
+
+	private void changeHiddenLayerTwoSize(int newHiddenLayerTwoSize) {
 		if (newHiddenLayerTwoSize <= 0) {
 			throw new ArithmeticException("layers must have more than one node");
 		} else {
 			hiddenLayerTwoSize = newHiddenLayerTwoSize;
 		}
 	}
-	private static void changeOutputLayerSize (int newOutputLayerSize) {
+
+	private void changeOutputLayerSize(int newOutputLayerSize) {
 		if (newOutputLayerSize <= 0) {
 			throw new ArithmeticException("layers must have more than one node");
 		} else {
 			outputLayerSize = newOutputLayerSize;
 		}
 	}
-	
+
+	// private methods to change the minInput, maxInput, minOutput, and maxOutput
+	private void changeInputParameters(double min, double max) {
+		// if minInput > maxInput (min should always be less than max)
+		if (min > max) {
+			throw new ArithmeticException("minInput must always be less than maxInput");
+		} else {
+			minInput = min;
+			maxInput = max;
+		}
+	}
+
+	private void changeOutputParameters(double min, double max) {
+		// if minOutput > maxOutput (min should always be less than max)
+		if (min > max) {
+			throw new ArithmeticException("minInput must always be less than maxInput");
+		} else {
+			minOutput = min;
+			maxOutput = max;
+		}
+	}
+
 	// the instance of the Random class we will be using to initialize the ANN
-	static Random rand = new Random();
-	
-	// to randomize the values in an array between 0 and 1 (used for the biases and weights arrays)
-	private static void randomizeArr (double[] arr) {
+	Random rand = new Random();
+
+	// to randomize the values in an array between 0 and 1 (used for the biases
+	// arrays)
+	private void randomizeOneDimensionalArr(double[] arr) {
 		for (int i = 0; i < arr.length; i++) {
 			// generates a double on the interval [0.0, 1.0]
 			arr[i] = rand.nextDouble();
 		}
 	}
-	
-	public ANN (int inputLayerSize, int hiddenLayerOneSize, int hiddenLayerTwoSize, int outputLayerSize) {
-		// calls all the methods for changing the size of the layers. They will throw exceptions if illegal values are attempted
+
+	private void randomizeTwoDimensionalArr(double[][] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				arr[i][j] = rand.nextDouble();
+			}
+		}
+	}
+
+	public ANN(int inputLayerSize, int hiddenLayerOneSize, int hiddenLayerTwoSize, int outputLayerSize, double minInput,
+			double maxInput, double minOutput, double maxOutput) {
+		// calls all the methods for changing the size of the layers. They will throw
+		// exceptions if illegal values are attempted
 		changeInputLayerSize(inputLayerSize);
 		changeHiddenLayerOneSize(hiddenLayerOneSize);
 		changeHiddenLayerTwoSize(hiddenLayerTwoSize);
 		changeOutputLayerSize(outputLayerSize);
-		
-		// initializing and randomizing all the biases
-		double[] biasesLayerOne = new double[hiddenLayerOneSize];
-		randomizeArr (biasesLayerOne);
-		double[] biasesLayerTwo = new double[hiddenLayerTwoSize];
-		randomizeArr (biasesLayerTwo);
-		double[] biasesOutputLayer = new double[outputLayerSize];
-		randomizeArr (biasesOutputLayer);
-		
-		// initializing and randomizing all the weights
-		double[] weightsAfterInputLayer = new double[inputLayerSize * hiddenLayerOneSize];
-		randomizeArr (weightsAfterInputLayer);
-		double[] weightsAfterLayerOne = new double[hiddenLayerOneSize * hiddenLayerTwoSize];
-		randomizeArr (weightsAfterLayerOne);
-		double[] weightsAfterLayerTwo = new double[hiddenLayerTwoSize * outputLayerSize];
-		randomizeArr (weightsAfterLayerTwo);
+
+		// initializing and randomizing all the biases (they were already initialized as
+		// variables above, but now we are assigning them a length and thus a slot in
+		// memory)
+		biasesLayerOne = new double[hiddenLayerOneSize];
+		randomizeOneDimensionalArr(biasesLayerOne);
+		biasesLayerTwo = new double[hiddenLayerTwoSize];
+		randomizeOneDimensionalArr(biasesLayerTwo);
+		biasesOutputLayer = new double[outputLayerSize];
+		randomizeOneDimensionalArr(biasesOutputLayer);
+
+		// initializing and randomizing all the weights (they were already initialized
+		// as variables above, but now we are assigning them a length and thus a slot in
+		// memory)
+		weightsAfterInputLayer = new double[inputLayerSize][hiddenLayerOneSize];
+		randomizeTwoDimensionalArr(weightsAfterInputLayer);
+		weightsAfterLayerOne = new double[hiddenLayerOneSize][hiddenLayerTwoSize];
+		randomizeTwoDimensionalArr(weightsAfterLayerOne);
+		weightsAfterLayerTwo = new double[hiddenLayerTwoSize][outputLayerSize];
+		randomizeTwoDimensionalArr(weightsAfterLayerTwo);
+
+		/*
+		 * initializing all the nodes the nodes are the parts of each layer that will
+		 * hold values as the calculation is done (they were already initialized as
+		 * variables above, but now we are assigning them a length and thus a slot in
+		 * memory)
+		 * 
+		 * the input layer nodes will come from the user input and the output layer will
+		 * be a result of the calculation (so a blank output layer will be passed in by
+		 * the user and the actual outputs will be returned)
+		 */
+		hiddenLayerOneNodes = new double[hiddenLayerOneSize];
+		hiddenLayerTwoNodes = new double[hiddenLayerTwoSize];
+
+		// changes the min and max for normalization and denormalization
+		changeInputParameters(minInput, maxInput);
+		changeOutputParameters(minOutput, maxOutput);
+	}
+
+	// the sigmoid method
+	private double sigmoid(double x) {
+		// Math.exp(x) is equivalent to (e^x)
+		return (1 / (1 + (1 / Math.exp(x))));
+	}
+
+	// the method that will calculate a single layer of node values
+	private void layerCalculate(double[] inputs, double[] targets, double[][] weights, double[] biases) {
+		for (int i = 0; i < targets.length; i++) {
+			// this will be the sum of all the inputs times all the weights plus a bias
+			double sum = 0;
+			for (int j = 0; j < inputs.length; j++) {
+				sum += (inputs[j] * weights[j][i]);
+			}
+			// add the bias to "sum"
+			sum += biases[i];
+			// reasign the "sum" variable as the sigmoid of "sum"
+			sum = sigmoid(sum);
+			// assign the output to the target array after passing "sum" through the sigmoid
+			// method
+			targets[i] = sum;
+		}
 	}
 	
-	public double calculate () {
-		double value = 0;
-		return value;
+	// the method that will normalize the input array
+	private void normalize(double[] arr) {
+		// do the normalization on each element of arr[]
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = ((arr[i] - minInput) / (maxInput - minInput));
+		}
+	}
+
+	// the method that will denormalize the output array
+	private void denormalize(double[] arr) {
+		// do the denormalization on each element of arr[]
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = (((maxOutput - minOutput) * arr[i]) + minOutput);
+		}
+	}
+
+	/*
+	 * this is the method that will do the actual calculation of the ANN
+	 * 
+	 * it takes in an array of inputs and a blank array for the outputs it will
+	 * change the values in the blank array for the outputs
+	 */
+	public void calculate(double[] inputs, double[] outputs) {
+		if ((inputs.length != inputLayerSize) || (outputs.length != outputLayerSize)) {
+			throw new ArithmeticException("You have not given the correct number of inputs and/or outputs");
+		} else {
+			// normalize the inputs
+			normalize(inputs);
+
+			// this will change all the values in "hiddenLayerOneNodes" to the calculated
+			// values
+			layerCalculate(inputs, hiddenLayerOneNodes, weightsAfterInputLayer, biasesLayerOne);
+			// this will change all the values in "hiddenLayerTwoNodes" to the calculated
+			// values
+			layerCalculate(hiddenLayerOneNodes, hiddenLayerTwoNodes, weightsAfterLayerOne, biasesLayerTwo);
+			// this will change all the values in "outputs" to the calculated values
+			layerCalculate(hiddenLayerTwoNodes, outputs, weightsAfterLayerTwo, biasesOutputLayer);
+			// by this part in the code, the outputs[] array should have been changed to the
+			// desired outputs (but still in normalized form)
+
+			// denormalize the outputs
+			denormalize(outputs);
+
+			System.out.println("=====================================================");
+			System.out.println("the inputs array " + arrToString(inputs));
+			System.out.println("the outputs array after calculation" + arrToString(outputs));
+			System.out.println(hiddenLayerOneNodes[3]);
+			System.out.println("=====================================================");
+		}
 	}
 }
--- ANNDemoTwo/src/Caller.java
+++ ANNDemoTwo/src/Caller.java
@@ -1,3 +1,23 @@
 public class Caller {
+	// the main() method
+	public static void main(String args[]) {
+		// an instance of the ANN class (from the ANN.java file)
+		ANN firstANN = new ANN(1, 5, 5, 1, 0, 1000, 0, 1000);
 
+		/*
+		 * the input array for the NN
+		 * 
+		 * we are defining it and assigning it values here since we are assigning it
+		 * values here, the size of the array is automatically assigned according to how
+		 * many values are passed in
+		 */
+		double[] inputs = new double[] {87};
+
+		// the output array for the NN
+		// we must assign the outputs[] array a size for the NN to work
+		double[] outputs = new double[1];
+
+		// using our instance of an ANN to calculate outputs based on the inputs
+		firstANN.calculate(inputs, outputs);
+	}
 }
